5d745b4e49ea6ce3eea43519fc00350aecda858c
Volodymyr <diachenko.vladimir@gmail.com>
2021-11-07 02:19:56 +0200

    Feat: Add Listing User types, add more object to seed function and add clear func for droping data from MongoDB.

--- server/src/lib/types.ts
+++ server/src/lib/types.ts
@@ -1,17 +1,62 @@
 import { Collection, ObjectId } from 'mongodb';
 
+export enum ListingType {
+  Apartment = 'APARTMENT',
+  House = 'HOUSE'
+}
+
+export interface BookingIndexMonth {
+  [key: string]: boolean
+}
+
+export interface BookingIndexYear {
+  [key: string]: BookingIndexMonth
+}
+
+export interface BookingsIndex {
+  [key: string]: BookingIndexYear
+}
+
+export interface Booking {
+  _id: ObjectId,
+  listing: ObjectId,
+  tenant: string,
+  checkIn: string,
+  checkOut: string
+}
+
 export interface Listing {
   _id: ObjectId,
   title: string,
+  description: string,
   image: string,
+  host: string,
   address: string,
+  country: string,
+  city: string,
+  admin: string,
+  bookings: ObjectId[],
   price: number,
-  numOfGuests: number,
-  numOfBeds: number,
-  numOfBaths: number,
-  rating: number
+  numOfGuests: number
+  type: ListingType,
+  bookingsIndex: BookingsIndex
+
+}
+
+export interface User {
+  _id: string,
+  token: string,
+  name: string,
+  avatar: string,
+  contact: string,
+  walletId?: string,
+  income: number,
+  bookings: ObjectId[],
+  listings: ObjectId[]
 }
 
 export interface Database {
+  bookings: Collection<Booking>;
   listings: Collection<Listing>;
+  users: Collection<User>;
 }
